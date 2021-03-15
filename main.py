bfd12821343529914b49440e5deb8ade65228e51
clvrk <jheru711@Gmail.com>
2021-03-15 11:48:51 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+import aiohttp
+
+load_dotenv()
+
+token = os.getenv('TOKEN')
+client = commands.AutoShardedBot(command_prefix=commands.when_mentioned_or('nms!'), case_insensitive=True)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} is online.')
+
+
+extensions = ['Cogs.planetinfo']
+
+for ext in extensions:
+    client.load_extension(ext)
+
+
+client.run(token)
