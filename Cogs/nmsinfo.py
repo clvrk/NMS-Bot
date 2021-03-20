061628304af039bf4b0021b9f4cfd8a34b96e803
Heru J <jheru711@gmail.com>
2021-03-20 15:09:38 -0700

    Update nmsinfo.py

--- Cogs/nmsinfo.py
+++ Cogs/nmsinfo.py
@@ -7,9 +7,5 @@ class planetinfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command() ## getting info out of an NMS wiki
-    async def info(self, ctx, *, query: str):
-    	
-
 def setup(bot):
     bot.add_cog(planetinfo(bot))
